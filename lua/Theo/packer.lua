9277aa8e0147c9fbbc1da44c833353c7f8ebde74
Theo <theokoss2002@gmail.com>
2024-09-05 17:09:12 -0500

    Change

--- lua/Theo/packer.lua
+++ lua/Theo/packer.lua
@@ -41,5 +41,6 @@ return require('packer').startup(function(use)
 	use('saadparwaiz1/cmp_luasnip')
 	use('hrsh7th/cmp-nvim-lua')
 	use('L3MON4D3/LuaSnip')
+
 	use('rafamadriz/friendly-snippets')
 end)
