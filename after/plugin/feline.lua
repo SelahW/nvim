4bfd9cae6ce4199df4e678584b011b6c362e38df
Theo <theokoss2002@gmail.com>
2024-09-06 16:07:21 -0500

    Added Feline and changed some remaps

---
+++ after/plugin/feline.lua
@@ -0,0 +1,19 @@
+local gruvbox = {
+    fg = '#928374',
+    bg = '#282828',
+    black ='#1d2021',
+    skyblue = '#458588',
+    cyan = '#83a597',
+    green = '#689d6a',
+    oceanblue = '#1d2021',
+    magenta = '#fb4934',
+    orange = '#fabd2f',
+    red = '#cc241d',
+    violet = '#b16286',
+    white = '#ebdbb2',
+    yellow = '#d79921',
+}
+local feline = require('feline')
+feline.setup({
+    theme = gruvbox
+})
