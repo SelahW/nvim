5d89ae3a4d9ca94862bc4340fb671ae15c6266d7
Theo <theokoss2002@gmail.com>
2024-09-13 11:57:37 -0500

    Added comments to remaps

--- lua/Theo/remap.lua
+++ lua/Theo/remap.lua
@@ -8,27 +8,28 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Open terminal in split buffer.
+-- Open terminal in split buffer
 vim.keymap.set("n", "<leader>t", "<cmd>vsplit<CR><C-w>l<cmd>terminal<CR>i")
+-- Paste without overwriting register
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- Copy to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- Copy line to clipboard
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
+-- Delete without overwriting register
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+-- Unbind Q
 vim.keymap.set("n", "Q", "<nop>")
+-- Format buffer depending on lsp
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- Replace all instances of word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
+-- Source file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
--- lua/Theo/set.lua
+++ lua/Theo/set.lua
@@ -28,9 +28,9 @@ vim.opt.inccommand = 'split'
 vim.opt.showmode = false
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
