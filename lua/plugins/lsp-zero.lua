051f48b026f1713342bea8df11c80179d1c27b0f
Theo <theokoss2002@gmail.com>
2024-09-21 13:56:28 -0500

    Fixed lsp-zero and mason

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -23,21 +23,6 @@ return {
             vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
             vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         end)
-
-        -- to learn how to use mason.nvim with lsp-zero
-        -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            ensure_installed = {'texlab'},
-            handlers = {
-                lsp_zero.default_setup,
-                lua_ls = function()
-                    local lua_opts = lsp_zero.nvim_lua_ls()
-                    require('lspconfig').lua_ls.setup(lua_opts)
-                end,
-            }
-        })
-
         local cmp = require('cmp')
         local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
